56e54ae4590f71870a6f801adda07fa86dbfae97
kwmiebach <kwmiebach@users.noreply.github.com>
2017-09-23 20:22:09 +0200

    Update let.py

--- let.py
+++ let.py
@@ -1,20 +1,5 @@
 """
 Home: https://github.com/kwmiebach/python-let
-Inspired by: https://nvbn.github.io/2014/09/25/let-statement-in-python/
-See discussion: https://www.reddit.com/r/Python/comments/41yr58/let_statement_in_python_2014/
-
-This is not a real let-statement, because the variables are only
-added to a dictionary that is available inside the with-block.
-
-The name of the dictionary will still overwrite anything with the same name
-outside the with-block. Only the values of the dictionary are
-unvavailable outside the block, they are explicitly deleted.
-
-A real let-statement which accesses pythons local variables seems difficult, since
-the tools that python provides, namely "inspect" and "locals()",
-do not or do not always allow write access. Only from the global scope 
-"locals()" delivers a dictionary that you can write back to.
-But a let-statement, that does not work inside a function seems of little use. 
 
 Usage example:
     
